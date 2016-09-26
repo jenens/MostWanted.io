04d662fe9409f73a3d25e4c239194b6b8d6b5710
Ridzick81613 <MRytz55@gmail.com>
2016-09-26 16:31:29 -0500

    Starting to successfully link up all the questions

--- app.js
+++ app.js
@@ -1,267 +1,267 @@
 var dataObject = { 
 	"272822514" : { 
-	  "firstName": "Billy",
-	  "lastName": "Bob",
-	  "gender": "male",
-	  "dob": "1/18/1949",
-	  "height": 71,
-	  "weight": 175,
-	  "eyeColor": "brown",
-	  "occupation": "programmer",
-	  "parents": [],
-	  "currentSpouse": 401222887 
+		"firstName": "Billy",
+		"lastName": "Bob",
+		"gender": "male",
+		"dob": "1/18/1949",
+		"height": 71,
+		"weight": 175,
+		"eyeColor": "brown",
+		"occupation": "programmer",
+		"parents": [],
+		"currentSpouse": 401222887 
 	},
 	"401222887" : { 
-	  "firstName": "Uma",
-	  "lastName": "Bob",
-	  "gender": "female",
-	  "dob": "4/1/1947",
-	  "height": 65,
-	  "weight": 162,
-	  "eyeColor": "brown",
-	  "occupation": "assistant",
-	  "parents": [],
-	  "currentSpouse": 272822514 
+		"firstName": "Uma",
+		"lastName": "Bob",
+		"gender": "female",
+		"dob": "4/1/1947",
+		"height": 65,
+		"weight": 162,
+		"eyeColor": "brown",
+		"occupation": "assistant",
+		"parents": [],
+		"currentSpouse": 272822514 
 	},
 	"409574486" : { 
-	  "firstName": "Michael",
-	  "lastName": "Walkens",
-	  "gender": "male",
-	  "dob": "5/9/1951",
-	  "height": 76,
-	  "weight": 250,
-	  "eyeColor": "brown",
-	  "occupation": "landscaper",
-	  "parents": [],
-	  "currentSpouse": 260451248 
+		"firstName": "Michael",
+		"lastName": "Walkens",
+		"gender": "male",
+		"dob": "5/9/1951",
+		"height": 76,
+		"weight": 250,
+		"eyeColor": "brown",
+		"occupation": "landscaper",
+		"parents": [],
+		"currentSpouse": 260451248 
 	},
 	"260451248" : {
-	  "firstName": "Jon",
-	  "lastName": "Walkens",
-	  "gender": "male",
-	  "dob": "9/6/1945",
-	  "height": 62,
-	  "weight": 115,
-	  "eyeColor": "brown",
-	  "occupation": "assistant",
-	  "parents": [],
-	  "currentSpouse": 409574486 
+		"firstName": "Jon",
+		"lastName": "Walkens",
+		"gender": "male",
+		"dob": "9/6/1945",
+		"height": 62,
+		"weight": 115,
+		"eyeColor": "brown",
+		"occupation": "assistant",
+		"parents": [],
+		"currentSpouse": 409574486 
 	},
 	"629807187" : {
-	  "firstName": "Jack",
-	  "lastName": "Pafoy",
-	  "gender": "male",
-	  "dob": "3/16/1938",
-	  "height": 70,
-	  "weight": 207,
-	  "eyeColor": "black",
-	  "occupation": "nurse",
-	  "parents": [],
-	  "currentSpouse": 464142841 
+		"firstName": "Jack",
+		"lastName": "Pafoy",
+		"gender": "male",
+		"dob": "3/16/1938",
+		"height": 70,
+		"weight": 207,
+		"eyeColor": "black",
+		"occupation": "nurse",
+		"parents": [],
+		"currentSpouse": 464142841 
 	},
 	"464142841" : {
-	  "firstName": "Jen",
-	  "lastName": "Pafoy",
-	  "gender": "female",
-	  "dob": "4/10/1940",
-	  "height": 72,
-	  "weight": 256,
-	  "eyeColor": "black",
-	  "occupation": "student",
-	  "parents": [],
-	  "currentSpouse": 629807187 
+		"firstName": "Jen",
+		"lastName": "Pafoy",
+		"gender": "female",
+		"dob": "4/10/1940",
+		"height": 72,
+		"weight": 256,
+		"eyeColor": "black",
+		"occupation": "student",
+		"parents": [],
+		"currentSpouse": 629807187 
 	},
 	"982411429" : {
-	  "firstName": "Mister",
-	  "lastName": "Potatoo",
-	  "gender": "male",
-	  "dob": "12/18/1952",
-	  "height": 66,
-	  "weight": 170,
-	  "eyeColor": "hazel",
-	  "occupation": "architect",
-	  "parents": [],
-	  "currentSpouse": 595767575 
+		"firstName": "Mister",
+		"lastName": "Potatoo",
+		"gender": "male",
+		"dob": "12/18/1952",
+		"height": 66,
+		"weight": 170,
+		"eyeColor": "hazel",
+		"occupation": "architect",
+		"parents": [],
+		"currentSpouse": 595767575 
 	},
 	"595767575" : {
-	  "firstName": "Missuz",
-	  "lastName": "Potatoo",
-	  "gender": "female",
-	  "dob": "10/28/1948",
-	  "height": 59,
-	  "weight": 137,
-	  "eyeColor": "blue",
-	  "occupation": "architect",
-	  "parents": [],
-	  "currentSpouse": 982411429 
+		"firstName": "Missuz",
+		"lastName": "Potatoo",
+		"gender": "female",
+		"dob": "10/28/1948",
+		"height": 59,
+		"weight": 137,
+		"eyeColor": "blue",
+		"occupation": "architect",
+		"parents": [],
+		"currentSpouse": 982411429 
 	},
 	"693243224" : {
-	  "firstName": "Joy",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "4/20/1939",
-	  "height": 69,
-	  "weight": 199,
-	  "eyeColor": "hazel",
-	  "occupation": "doctor",
-	  "parents": [],
-	  "currentSpouse": 693243224 
+		"firstName": "Joy",
+		"lastName": "Madden",
+		"gender": "female",
+		"dob": "4/20/1939",
+		"height": 69,
+		"weight": 199,
+		"eyeColor": "hazel",
+		"occupation": "doctor",
+		"parents": [],
+		"currentSpouse": 693243224 
 	},
 	"888201200" : {
-	  "firstName": "Mader",
-	  "lastName": "Madden",
-	  "gender": "male",
-	  "dob": "5/6/1937",
-	  "height": 76,
-	  "weight": 205,
-	  "eyeColor": "black",
-	  "occupation": "landscaper",
-	  "parents": [],
-	  "currentSpouse": 888201200 
+		"firstName": "Mader",
+		"lastName": "Madden",
+		"gender": "male",
+		"dob": "5/6/1937",
+		"height": 76,
+		"weight": 205,
+		"eyeColor": "black",
+		"occupation": "landscaper",
+		"parents": [],
+		"currentSpouse": 888201200 
 	},
 	"878013758" : {
-	  "firstName": "Jill",
-	  "lastName": "Pafoy",
-	  "gender": "Bob",
-	  "dob": "2/8/1972",
-	  "height": 74,
-	  "weight": 118,
-	  "eyeColor": "brown",
-	  "occupation": "programmer",
-	  "parents": [401222887,401222887],
-	  "currentSpouse": 294874671 
+		"firstName": "Jill",
+		"lastName": "Pafoy",
+		"gender": "Bob",
+		"dob": "2/8/1972",
+		"height": 74,
+		"weight": 118,
+		"eyeColor": "brown",
+		"occupation": "programmer",
+		"parents": [401222887,401222887],
+		"currentSpouse": 294874671 
 	},
 	"951747547" : {
-	  "firstName": "Ralph",
-	  "lastName": "Bob",
-	  "gender": "male",
-	  "dob": "12/23/1969",
-	  "height": 66,
-	  "weight": 179,
-	  "eyeColor": "blue",
-	  "occupation": "nurse",
-	  "parents": [401222887,401222887],
-	  "currentSpouse": 159819275 
+		"firstName": "Ralph",
+		"lastName": "Bob",
+		"gender": "male",
+		"dob": "12/23/1969",
+		"height": 66,
+		"weight": 179,
+		"eyeColor": "blue",
+		"occupation": "nurse",
+		"parents": [401222887,401222887],
+		"currentSpouse": 159819275 
 	},
 	"159819275" : {
-	  "firstName": "Jasmine",
-	  "lastName": "Bob",
-	  "gender": "female",
-	  "dob": "12/18/1969",
-	  "height": 58,
-	  "weight": 156,
-	  "eyeColor": "blue",
-	  "occupation": "assistant",
-	  "parents": [409574486,260451248],
-	  "currentSpouse": 951747547 
+		"firstName": "Jasmine",
+		"lastName": "Bob",
+		"gender": "female",
+		"dob": "12/18/1969",
+		"height": 58,
+		"weight": 156,
+		"eyeColor": "blue",
+		"occupation": "assistant",
+		"parents": [409574486,260451248],
+		"currentSpouse": 951747547 
 	},
 	"348457184" : {
-	  "firstName": "Annie",
-	  "lastName": "Pafoy",
-	  "gender": "female",
-	  "dob": "11/4/1970",
-	  "height": 62,
-	  "weight": 235,
-	  "eyeColor": "hazel",
-	  "occupation": "landscaper",
-	  "parents": [629807187,464142841],
-	  "currentSpouse": null 
+		"firstName": "Annie",
+		"lastName": "Pafoy",
+		"gender": "female",
+		"dob": "11/4/1970",
+		"height": 62,
+		"weight": 235,
+		"eyeColor": "hazel",
+		"occupation": "landscaper",
+		"parents": [629807187,464142841],
+		"currentSpouse": null 
 	},
 	"294874671" : {
-	  "firstName": "Dave",
-	  "lastName": "Pafoy",
-	  "gender": "male",
-	  "dob": "8/5/1967",
-	  "height": 61,
-	  "weight": 112,
-	  "eyeColor": "green",
-	  "occupation": "doctor",
-	  "parents": [629807187,464142841],
-	  "currentSpouse": 878013758 
+		"firstName": "Dave",
+		"lastName": "Pafoy",
+		"gender": "male",
+		"dob": "8/5/1967",
+		"height": 61,
+		"weight": 112,
+		"eyeColor": "green",
+		"occupation": "doctor",
+		"parents": [629807187,464142841],
+		"currentSpouse": 878013758 
 	},
 	"931247228" : {
-	  "firstName": "Amii",
-	  "lastName": "Pafoy",
-	  "gender": "female",
-	  "dob": "3/13/1963",
-	  "height": 74,
-	  "weight": 184,
-	  "eyeColor": "brown",
-	  "occupation": "landscaper",
-	  "parents": [629807187,464142841],
-	  "currentSpouse": null 
+		"firstName": "Amii",
+		"lastName": "Pafoy",
+		"gender": "female",
+		"dob": "3/13/1963",
+		"height": 74,
+		"weight": 184,
+		"eyeColor": "brown",
+		"occupation": "landscaper",
+		"parents": [629807187,464142841],
+		"currentSpouse": null 
 	},
 	"822843554" : {
-	  "firstName": "Regina",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "7/26/1959",
-	  "height": 71,
-	  "weight": 249,
-	  "eyeColor": "brown",
-	  "occupation": "nurse",
-	  "parents": [693243224, 888201200],
-	  "currentSpouse": null 
+		"firstName": "Regina",
+		"lastName": "Madden",
+		"gender": "female",
+		"dob": "7/26/1959",
+		"height": 71,
+		"weight": 249,
+		"eyeColor": "brown",
+		"occupation": "nurse",
+		"parents": [693243224, 888201200],
+		"currentSpouse": null 
 	},
 	"819168108" : {
-	  "firstName": "Hana",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "10/7/1953",
-	  "height": 70,
-	  "weight": 187,
-	  "eyeColor": "brown",
-	  "occupation": "politician",
-	  "parents": [693243224, 888201200],
-	  "currentSpouse": null 
+		"firstName": "Hana",
+		"lastName": "Madden",
+		"gender": "female",
+		"dob": "10/7/1953",
+		"height": 70,
+		"weight": 187,
+		"eyeColor": "brown",
+		"occupation": "politician",
+		"parents": [693243224, 888201200],
+		"currentSpouse": null 
 	},
 	"969837479" : {
-	  "firstName": "Eloise",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "12/11/1961",
-	  "height": 63,
-	  "weight": 241,
-	  "eyeColor": "brown",
-	  "occupation": "assistant",
-	  "parents": [693243224, 888201200],
-	  "currentSpouse": null 
+		"firstName": "Eloise",
+		"lastName": "Madden",
+		"gender": "female",
+		"dob": "12/11/1961",
+		"height": 63,
+		"weight": 241,
+		"eyeColor": "brown",
+		"occupation": "assistant",
+		"parents": [693243224, 888201200],
+		"currentSpouse": null 
 	},
 	"313207561" : {
-	  "firstName": "Mattias",
-	  "lastName": "Madden",
-	  "gender": "male",
-	  "dob": "2/19/1966",
-	  "height": 70,
-	  "weight": 110,
-	  "eyeColor": "blue",
-	  "occupation": "assistant",
-	  "parents": [693243224, 888201200],
-	  "currentSpouse": 313997561 
+		"firstName": "Mattias",
+		"lastName": "Madden",
+		"gender": "male",
+		"dob": "2/19/1966",
+		"height": 70,
+		"weight": 110,
+		"eyeColor": "blue",
+		"occupation": "assistant",
+		"parents": [693243224, 888201200],
+		"currentSpouse": 313997561 
 	},
 	"313997561" : {
-	  "firstName": "Ellen",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "2/19/1970",
-	  "height": 67,
-	  "weight": 100,
-	  "eyeColor": "blue",
-	  "occupation": "doctor",
-	  "parents": [],
-	  "currentSpouse": 313207561 
+		"firstName": "Ellen",
+		"lastName": "Madden",
+		"gender": "female",
+		"dob": "2/19/1970",
+		"height": 67,
+		"weight": 100,
+		"eyeColor": "blue",
+		"occupation": "doctor",
+		"parents": [],
+		"currentSpouse": 313207561 
 	},
 	"313998000" : {
-	  "firstName": "Joey",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "2/02/1987",
-	  "height": 67,
-	  "weight": 100,
-	  "eyeColor": "blue",
-	  "occupation": "doctor",
-	  "parents": [313207561,313997561],
-	  "currentSpouse": null 
+		"firstName": "Joey",
+		"lastName": "Madden",
+		"gender": "female",
+		"dob": "2/02/1987",
+		"height": 67,
+		"weight": 100,
+		"eyeColor": "blue",
+		"occupation": "doctor",
+		"parents": [313207561,313997561],
+		"currentSpouse": null 
 	}
 };
 
@@ -280,32 +280,43 @@ function initSearch(){
 	alert("Hello World");
 
 	// get all the information you need to run the search
-	var firstAnswer = prompt("Do you know who you want to search for?");
-		if (firstAnswer == "yes") {
-			 followUpQuestion();
-		}
+	var searchFirstName = prompt("Do you know the first name of who you are searching for? (yes or no)");
+	if (searchFirstName.toLowerCase() == "yes") {
+		followUpQuestion();
+	} else if (searchFirstName.toLowerCase == "no"){
+
+	}
+
 
 
 	// then pass that info to the respective function.
 
-	var result = getPersonInfo("J", "T")
+	// var result = getPersonInfo("J", "T");
 
 	// once the search is done, pass the results to the responder function
-	responder(result);
+	// responder(result);
 }
 
 function followUpQuestion(){
-	var secondAnswer = prompt("Do you know their first name?")
-		if(secondAnswer == "yes") {
-			followUpQuestion2();
-		}
+	var searchLastName = prompt("Do you know their last name? (yes or no)")
+	if(searchLastName.toLowerCase() == "yes") {
+		followUpQuestion2();
+	}
+	else if(searchLastName.toLowerCase =="no") {
+		followUpQuestion2a()
+	}
+
 }
 
+// function followUpQuestion2a() {
+// 	var 
+// }
+
 function followUpQuestion2(){
-	var thirdAnswer = prompt("Please enter first name.")
-		if (thirdAnswer == Billy) {
-			getPersonInfo();
-		}
+	var enterFirstName = prompt("Please enter first name.")
+	if (enterFirstName.toLowerCase == Billy) {
+		getPersonInfo();
+	}
 }
 
 function responder(results){
@@ -326,4 +337,4 @@ function getFamily(){
 // there will be much more here, and some of the code above will certainly change
 
 initSearch();
-window.close(); // exit window as the end of the session -- you may remove this
+// window.close(); // exit window as the end of the session -- you may remove this
